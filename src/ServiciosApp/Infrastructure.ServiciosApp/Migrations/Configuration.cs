2dbf8893c75668f3364744f6ad0acb35602869e6
gapalmas <gapalmas@outlook.com>
2026-02-04 15:02:10 -0700

    Enable Migrations

---
+++ src/ServiciosApp/Infrastructure.ServiciosApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Infrastructure.ServiciosApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Infrastructure.ServiciosApp.Data.SqlDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Infrastructure.ServiciosApp.Data.SqlDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
